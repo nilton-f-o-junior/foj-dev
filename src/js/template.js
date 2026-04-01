860fdf989d2ea0e6599c6516ab93c60191ff8cce
nilton-f-o-junior <nilton.f.o.junior@gmail.com>
2026-04-01 00:24:46 -0300

    docs: adição do artigo - fluxo diario

--- src/js/template.js
+++ src/js/template.js
@@ -1,25 +1,23 @@
 function copyCode(button) {
+  const container = button.closest(".code-container");
+  const code = container.querySelector("code").innerText;
 
-  const container = button.closest('.code-container');
-  const code = container.querySelector('code').innerText;
+  navigator.clipboard
+    .writeText(code)
+    .then(() => {
+      button.innerText = "✓ Copiado";
+      button.classList.add("copied");
 
-
-  navigator.clipboard.writeText(code).then(() => {
-
-    button.innerText = "✓ Copiado";
-    button.classList.add('copied');
-
-
-    setTimeout(() => {
-      button.innerText = "Copiar";
-      button.classList.remove('copied');
-    }, 2000);
-  }).catch(err => {
-
-    console.error('Erro ao copiar:', err);
-    button.innerText = "Erro";
-    setTimeout(() => {
-      button.innerText = "Copiar";
-    }, 2000);
-  });
+      setTimeout(() => {
+        button.innerText = "Copiar";
+        button.classList.remove("copied");
+      }, 2000);
+    })
+    .catch((err) => {
+      console.error("Erro ao copiar:", err);
+      button.innerText = "Erro";
+      setTimeout(() => {
+        button.innerText = "Copiar";
+      }, 2000);
+    });
 }
